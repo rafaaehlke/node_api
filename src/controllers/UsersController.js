b929bc6d4d764a448a86323333b207a5e4051f13
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-11-22 16:09:28 -0300

    :construction: Faz validação de nome e e-mail ao atualizar um registro no banco de dados utilizando o Nullish coalescing operator

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -42,8 +42,8 @@ class UsersController{
       throw new AppError("Este email já está em uso.")
     }
 
-    user.name = name;
-    user.email = email;
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
     
     // Se o usuário digitar apenas a senha nova, sem a antiga apresentará a mensagem abaixo
     if(password && !old_password) {
@@ -68,9 +68,9 @@ class UsersController{
       name = ?,
       email = ?,
       password = ?,
-      updated_at = ?
+      updated_at = DATETIME('now')
       WHERE id = ?`,
-      [user.name, user.email, user.password, new Date(), id]  
+      [user.name, user.email, user.password, id]  
       )
 
       return response.json()
