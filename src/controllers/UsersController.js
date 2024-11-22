712bd9c641226d4a1f87af208ff0837570cca564
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-11-22 16:01:31 -0300

    :card_file_box: Atualizaçao Password User

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,7 @@
-const { hash } = require("bcryptjs")
+const { hash , compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
-
 const sqliteConnection = require("../database/sqlite")
+
 
 class UsersController{
  async create(request, response) {
@@ -21,6 +21,59 @@ class UsersController{
     )
 
     return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+    if(!user) {
+      throw new AppError("Usuário não encontrado.")
+    }
+
+    // Verifica se já existe um usuário com o e-mail especificado
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    // Verifica se o id do usuário é diferente do id do email, se for diferente nao deixa alterar
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este email já está em uso.")
+    }
+
+    user.name = name;
+    user.email = email;
+    
+    // Se o usuário digitar apenas a senha nova, sem a antiga apresentará a mensagem abaixo
+    if(password && !old_password) {
+      throw new AppError("Voce precisa informar a senha antiga!")
+    }
+
+    // Compara a senha nova com a antiga
+    if(password && old_password) {
+      // Checa se a senha antiga bate com a que era antes
+      const checkOldPassword = await compare(old_password, user.password)
+      // Se a senha antiga for diferente, apresentará o erro a abaixo
+      if(!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.")
+      }
+
+      // Faz a criptografia da senha nova para a DB 
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, user.password, new Date(), id]  
+      )
+
+      return response.json()
   }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,5 +8,6 @@ const usersRoutes = Router()
 const userController = new UsersController()
 
 usersRoutes.post("/", userController.create)
+usersRoutes.put("/:id", userController.update)
 
 module.exports = usersRoutes
