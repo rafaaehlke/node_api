d615e6a387f709e1fcbb4c5bbdb942e177c71af8
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-11-22 13:17:38 -0300

    :construction: Criado tratamento de erros e adicionado biblioteca express async errors

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,14 @@
+const AppError = require("../utils/AppError")
+
+
 class UsersController{
   create(request, response) {
     const { name, email, password } = request.body
 
-    response.json( {name, email} )
+    if(!name){
+      throw new AppError("O nome é obrigatório!")
+    }
+    response.json( {name, email, password} )
   }
 }
 
