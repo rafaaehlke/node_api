9517f1c23fe56bd77077cea7401c4ec7636ebe61
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-11-22 15:02:18 -0300

    :card_file_box: Cadastro Users na DB

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -13,6 +13,10 @@ class UsersController{
       throw new AppError("Este email já está em uso.")
     }
 
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",[name, email, password]
+    )
+
     return response.status(201).json()
   }
 }
