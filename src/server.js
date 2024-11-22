f7eac39cdbd52c58045d0ae49c04848d486822d0
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-11-22 14:42:30 -0300

    :card_file_box: Adicionado Migrations

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 require("express-async-errors")
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError")
 const express = require("express")
 
 const routes = require("./routes")
 
+migrationsRun()
+
 const app = express()
 app.use(express.json())
 
 app.use(routes)
-
-database()
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
@@ -28,7 +28,6 @@ app.use((error, request, response, next) => {
     message: "Internal server error"
   })
 })
-
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`))
