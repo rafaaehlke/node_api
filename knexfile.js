010942d570f4f8ec3a7320ee3ec5c739feb3a29d
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-11-22 17:10:58 -0300

    :construction: Migrations para NOTES

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    }, 
     useNullAsDefault: true
   }
 };
