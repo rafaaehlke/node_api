c4d9c4fca65f37f115dc75f0ad8a512c3c520fe1
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-11-22 15:07:52 -0300

    :card_file_box: Criptografando Password na DB

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 
 const sqliteConnection = require("../database/sqlite")
@@ -13,8 +14,10 @@ class UsersController{
       throw new AppError("Este email já está em uso.")
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",[name, email, password]
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",[name, email, hashedPassword]
     )
 
     return response.status(201).json()
