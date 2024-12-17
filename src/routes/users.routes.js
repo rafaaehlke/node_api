7b8435f30b50ae79a57c7089b00910768d5f258e
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-12-16 22:10:41 -0300

    :construction: Salvando imagem na DB

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,19 +5,19 @@ const uploadConfig = require("../configs/upload")
 
 
 const UsersController = require("../controllers/UsersController")
+const UserAvatarController = require("../controllers/UserAvatarController.js")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated.js")
-const res = require("express/lib/response.js")
+
 
 const usersRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 const userController = new UsersController()
+const userAvatarController = new UserAvatarController()
+
 
 usersRoutes.post("/", userController.create)
 usersRoutes.put("/", ensureAuthenticated, userController.update)
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
-  console.log(request.file.filename)
-  response.json()
-})
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes
