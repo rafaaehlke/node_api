2b519f7070c71f4e298b345f5ddfdeae233939e5
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-01-16 19:38:30 -0300

    :construction: adicionado describe para agrupar o teste

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,18 +1,20 @@
 const UserCreateService = require("./UserCreateService")
-const UserRepositoryInMemory = require ("../repositories/UserRepositoryInMemory")
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
 
-it("user created sucessfull", async () => {
-  const user = {
-    name: "User Test",
-    email: "teste@email.com",
-    password: "123"
-  }
+describe("UserCreateService", () => {
+  it("user created sucessfull", async () => {
+    const user = {
+      name: "User Test",
+      email: "teste@email.com",
+      password: "123"
+    }
 
-  const userRepositoryInMemory = new UserRepositoryInMemory()
-  const userCreateService = new UserCreateService(userRepositoryInMemory)
-  const userCreated = await userCreateService.execute(user)
+    const userRepositoryInMemory = new UserRepositoryInMemory()
+    const userCreateService = new UserCreateService(userRepositoryInMemory)
+    const userCreated = await userCreateService.execute(user)
 
-  console.log(userCreated)
-  expect(userCreated).toHaveProperty("id")
+    console.log(userCreated)
+    expect(userCreated).toHaveProperty("id")
 
+  })
 })
